16fe0d95fd5bf3be61c6a17d199b65e81c8290fa
anonyme20 <65164908+anonyme20@users.noreply.github.com>
2020-06-04 16:42:01 +0200

    Update train_model_Dcc_Da.py

--- train_model_Dcc_Da.py
+++ train_model_Dcc_Da.py
@@ -179,8 +179,8 @@ if __name__ == "__main__":
                 Loss.append(loss)
 
             print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\t epoch : {epoch}, \t"
-                  f"class loss = {np.mean(np.asarray(Loss))}\t"
-                  f"Da loss = {np.mean(np.asarray(Loss_Da))}")
+                  f"Dcc = {np.mean(np.asarray(Loss))}\t"
+                  f"Da = {np.mean(np.asarray(Loss_Da))}")
 
             if epoch % save_each == 0 or epoch == args.epochs:
                 # model.save("%s/model_bce_epoch_%d" % (checkpoint_dir_, epoch))
